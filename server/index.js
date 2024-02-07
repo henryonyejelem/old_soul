7acfede39abf9ce107a695ee96af18c6670ae782
Henry Onyejelem <onyejelemco@gmail.com>
2024-02-06 22:36:01 -0600

    Henry Onyejelem

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(8000, () => {
+    console.log("Server is Running");
+})
